b1657686f6ab6048750c19a5469ae1d1908dd343
sgrigerc <sgrigerc@gmail.com>
2022-09-10 22:45:24 +0300

    start sample

---
+++ samplesite/bboard/models.py
@@ -0,0 +1,17 @@
+from tabnanny import verbose
+from django.db import models
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    
+    class Meta:
+        verbose_name_plural = 'Оъявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
+
+
+
+
