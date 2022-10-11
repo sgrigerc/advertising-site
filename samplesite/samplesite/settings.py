8f3ef34d18fc4f6a3e6cd3b9f4a003d00c4e2116
sgrigerc <sgrigerc@gmail.com>
2022-10-12 00:35:07 +0300

    Авторизация через гитхаб и гугл

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insehjjhvkghvjhvljj-fbfs-rbwq1n4@2yli&hd8)^j^atez'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -149,7 +149,6 @@ LOGIN_URL = 'login'
 from dotenv import load_dotenv
 load_dotenv()  # loads the configs from .env
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # social auth configs for github
 SOCIAL_AUTH_GITHUB_KEY = str(os.getenv('GITHUB_KEY'))
@@ -158,3 +157,10 @@ SOCIAL_AUTH_GITHUB_SECRET = str(os.getenv('GITHUB_SECRET'))
 # social auth configs for google
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = str(os.getenv('GOOGLE_KEY'))
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = str(os.getenv('GOOGLE_SECRET'))
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/',
              auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
              name='password_reset_confirm'),
+    path('password-reset-complete/',
+             auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+             name='password_reset_complete'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
+
 ]
